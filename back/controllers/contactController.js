11287ff10680b75e47f17a1c8e29bb5fa7d38f82
iamdamba <iamdamba720@gmail.com>
2021-10-25 17:38:15 +0200

    commit

--- back/controllers/contactController.js
+++ back/controllers/contactController.js
@@ -6,6 +6,8 @@ const home_url = process.env.PORT || 5000;
 const transporter = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
