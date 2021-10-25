154faf924230fa97337f49772ecf76b88bf45b94
iamdamba <iamdamba720@gmail.com>
2021-10-25 04:31:09 +0200

    Update server

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static("../front/build"));
 
 // ||||||||||||||||||||||||| Routes |||||||||||||||||||||||||||
 
@@ -37,6 +38,9 @@ app.use(paymentRoute);
 app.use(contactRoute);
 app.use(memberRoute);
 
+app.get("/*", (_, res) => {
+  res.sendFile(path.join(__dirname, "../front/build/index.html"));
+});
 // ||||||||||||||||||||||||| Listen |||||||||||||||||||||||||||
 
 mongoose
