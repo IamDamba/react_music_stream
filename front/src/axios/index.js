2c30f359b393b8746ef2bc7c10007d08597c0bca
iamdamba <iamdamba720@gmail.com>
2021-10-26 17:41:36 +0200

    Update 26/10/2021 - 17:37

--- front/src/axios/index.js
+++ front/src/axios/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.NODE_ENV
-    ? process.env.REACT_APP_BASE_URL_LOCAL
-    : process.env.REACT_APP_BASE_URL_PUBLIC,
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_BASE_URL_LOCAL
+      : process.env.REACT_APP_BASE_URL_PUBLIC,
 });
