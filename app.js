b0cb75bc68e2912d24cb516ea10408bb4b85b2b4
iamdamba <iamdamba720@gmail.com>
2021-10-26 18:04:42 +0200

    Update 26/10/2021 - 18:00

--- app.js
+++ app.js
@@ -31,11 +31,11 @@ app.use(express.urlencoded({ extended: true }));
 
 // ||||||||||||||||||||||||| Routes |||||||||||||||||||||||||||
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "front/build", "index.html"));
 });
 app.get("/api/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "back"));
+  res.sendFile(path.join(__dirname));
 });
 
 app.use("/api", authRoute);
