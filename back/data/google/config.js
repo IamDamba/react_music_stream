bf136f95433fa599225f314ca336aade7435a5ef
iamdamba <iamdamba720@gmail.com>
2021-10-26 15:50:31 +0200

    Update 26/10/2021 - 15:50

--- back/data/google/config.js
+++ back/data/google/config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  clientId:
-    "1056833024396-3ht04ag4fk39hm8gdcp78o4373qc3jq5.apps.googleusercontent.com",
-  clientSecret: "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",
-  refreshToken:
-    "1//044mBsvyEKd9FCgYIARAAGAQSNwF-L9IrPD04y7g_KBzPhPz_W9cGf0NqTE3ozLDzYujzOgyTQukv4Gw4iEbzqkH0IKGqJdb5-lE",
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  refreshToken: process.env.REFRESH_TOKEN,
   user_email: process.env.EMAIL_USER,
 };
