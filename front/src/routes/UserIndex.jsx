68388e7ed72efff7199279bc887cf0be08b71c55
iamdamba <iamdamba720@gmail.com>
2021-10-26 20:30:42 +0200

    Update 26/10/2021 - 20:30

--- front/src/routes/UserIndex.jsx
+++ front/src/routes/UserIndex.jsx
@@ -7,11 +7,14 @@ import Signin from "../pages/views/signin/Signin";
 import Signup from "../pages/views/signup/Signup";
 import Toast from "../pages/layouts/Toast";
 import Error404 from "../pages/views/error404/Error404";
+import AdBlockModal from "../pages/layouts/AdBlockModal.jsx";
 
+import { detectAnyAdblocker } from "just-detect-adblock";
 import { useSelector } from "react-redux";
 
 const UserIndex = () => {
-  //Hooks
+  //hooks
+  const [isAdBlockActive, setIsAdBlockActive] = useState(false);
   const [isUser, setIsUser] = useState(false);
 
   //Redux
@@ -39,6 +42,17 @@ const UserIndex = () => {
       });
   };
 
+  // Functions
+  useEffect(() => {
+    detectAnyAdblocker().then((detected) => {
+      if (detected) {
+        setIsAdBlockActive(true);
+      } else {
+        setIsAdBlockActive(false);
+      }
+    });
+  }, []);
+
   useEffect(() => {
     fetchUser();
   });
@@ -46,19 +60,27 @@ const UserIndex = () => {
   if (!isUser) {
     return <div></div>;
   } else {
-    return (
-      <Router>
-        <Toast position="bottom-left" />
-        <Switch>
-          <Route exact path="/signin" component={Signin} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/forget-password" component={ForgetPassword} />
-          <Route exact path="/:email/resetpassword" component={ResetPassword} />
-          {/*All Other Route*/}
-          <Route path="*" component={Error404} />
-        </Switch>
-      </Router>
-    );
+    if (isAdBlockActive) {
+      return <AdBlockModal />;
+    } else {
+      return (
+        <Router>
+          <Toast position="bottom-left" />
+          <Switch>
+            <Route exact path="/signin" component={Signin} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/forget-password" component={ForgetPassword} />
+            <Route
+              exact
+              path="/:email/resetpassword"
+              component={ResetPassword}
+            />
+            {/*All Other Route*/}
+            <Route path="*" component={Error404} />
+          </Switch>
+        </Router>
+      );
+    }
   }
 };
 
